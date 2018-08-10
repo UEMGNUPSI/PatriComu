04ebd9bca23a8b70346f1fd34a6c7f46ca4e1b2c
Leandro <Leandro@LAPTOP-MA11G6EE>
2018-08-10 15:11:15 -0300

    13º Commit LoginView reformulada.

--- src/VIEW/LoginView.java
+++ src/VIEW/LoginView.java
@@ -47,6 +47,7 @@ public class LoginView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
         txtSenha = new javax.swing.JPasswordField();
         jSeparator3 = new javax.swing.JSeparator();
@@ -57,7 +58,7 @@ public class LoginView extends javax.swing.JFrame {
         lblversão = new javax.swing.JLabel();
         lblErro = new javax.swing.JLabel();
         btnSair = new javax.swing.JLabel();
-        BackGround = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBounds(new java.awt.Rectangle(0, 0, 0, 0));
@@ -65,10 +66,14 @@ public class LoginView extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIEW/Icones Inativos/Senha.png"))); // NOI18N
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 550, -1, -1));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        txtSenha.setFont(new java.awt.Font("Champagne & Limousines", 0, 18)); // NOI18N
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIEW/Icones Inativos/Senha.png"))); // NOI18N
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 550, -1, -1));
+
+        txtSenha.setFont(new java.awt.Font("Segoe UI Light", 0, 18)); // NOI18N
+        txtSenha.setForeground(new java.awt.Color(32, 33, 41));
         txtSenha.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txtSenha.setBorder(null);
         txtSenha.addKeyListener(new java.awt.event.KeyAdapter() {
@@ -76,29 +81,31 @@ public class LoginView extends javax.swing.JFrame {
                 txtSenhaKeyPressed(evt);
             }
         });
-        getContentPane().add(txtSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 550, 137, 32));
+        jPanel1.add(txtSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 550, 137, 32));
 
-        jSeparator3.setForeground(new java.awt.Color(102, 102, 102));
-        getContentPane().add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 590, 202, 10));
+        jSeparator3.setForeground(new java.awt.Color(40, 50, 89));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 590, 202, 10));
 
-        jSeparator2.setForeground(new java.awt.Color(102, 102, 102));
-        getContentPane().add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 590, 202, 10));
+        jSeparator2.setForeground(new java.awt.Color(40, 50, 89));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 590, 202, 10));
 
         btnLogin.setBackground(new java.awt.Color(255, 255, 255));
-        btnLogin.setFont(new java.awt.Font("Champagne & Limousines", 1, 18)); // NOI18N
-        btnLogin.setText("Entrar");
-        btnLogin.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 153, 255), 1, true));
+        btnLogin.setFont(new java.awt.Font("Segoe UI Black", 0, 16)); // NOI18N
+        btnLogin.setForeground(new java.awt.Color(34, 38, 53));
+        btnLogin.setText("ENTRAR");
+        btnLogin.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 153, 255)));
         btnLogin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnLoginActionPerformed(evt);
             }
         });
-        getContentPane().add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 650, 93, 30));
+        jPanel1.add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 650, 110, 40));
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIEW/Icones Inativos/Login.png"))); // NOI18N
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 550, -1, -1));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 550, -1, -1));
 
-        txtUser.setFont(new java.awt.Font("Champagne & Limousines", 0, 18)); // NOI18N
+        txtUser.setFont(new java.awt.Font("Segoe UI Light", 0, 18)); // NOI18N
+        txtUser.setForeground(new java.awt.Color(32, 33, 41));
         txtUser.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txtUser.setBorder(null);
         txtUser.addKeyListener(new java.awt.event.KeyAdapter() {
@@ -106,17 +113,18 @@ public class LoginView extends javax.swing.JFrame {
                 txtUserKeyPressed(evt);
             }
         });
-        getContentPane().add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 550, 137, 30));
+        jPanel1.add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 550, 137, 30));
 
-        lblversão.setFont(new java.awt.Font("Champagne & Limousines", 0, 18)); // NOI18N
+        lblversão.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblversão.setForeground(new java.awt.Color(32, 33, 41));
         lblversão.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblversão.setText("Patrimônio Comunicação 0.8 Beta");
-        getContentPane().add(lblversão, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 246, -1));
+        jPanel1.add(lblversão, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 290, -1));
 
-        lblErro.setFont(new java.awt.Font("Champagne & Limousines", 1, 24)); // NOI18N
-        lblErro.setForeground(new java.awt.Color(192, 39, 39));
+        lblErro.setFont(new java.awt.Font("Segoe UI Light", 1, 22)); // NOI18N
+        lblErro.setForeground(new java.awt.Color(199, 65, 65));
         lblErro.setText("Senha ou usuário incorretos!");
-        getContentPane().add(lblErro, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 730, -1, -1));
+        jPanel1.add(lblErro, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 500, -1, -1));
 
         btnSair.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIEW/Icones Inativos/Fechar.png"))); // NOI18N
         btnSair.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -130,10 +138,15 @@ public class LoginView extends javax.swing.JFrame {
                 btnSairMouseExited(evt);
             }
         });
-        getContentPane().add(btnSair, new org.netbeans.lib.awtextra.AbsoluteConstraints(1340, 0, -1, -1));
+        jPanel1.add(btnSair, new org.netbeans.lib.awtextra.AbsoluteConstraints(1330, 10, -1, -1));
 
-        BackGround.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VIEW/Icones Inativos/tela iniciar login - Copia.png"))); // NOI18N
-        getContentPane().add(BackGround, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        jLabel1.setFont(new java.awt.Font("Segoe UI Light", 0, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(88, 145, 193));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Patrimonio Comunicação");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 1370, 140));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1370, 770));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -270,11 +283,12 @@ public class LoginView extends javax.swing.JFrame {
     }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel BackGround;
     private javax.swing.JButton btnLogin;
     private javax.swing.JLabel btnSair;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JLabel lblErro;
